27983bbb2a1699300c7d6e738951676e2b237df2
Thomas Aldrian <aldrian.thom@proton.me>
2026-03-03 14:07:11 +0000

    feat: errors

---
+++ include/errors/errors.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <format>
+#include <stdexcept>
+#include <string>
+
+namespace thogcc::errors {
+
+struct ErrorBase : public std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+struct CommandLineError : public ErrorBase {
+    CommandLineError(const std::string& msg)
+        : ErrorBase(std::format("CommandLineError: {}", msg)){};
+};
+
+struct ParseError : public ErrorBase {
+    ParseError(const std::string& msg) : ErrorBase(std::format("ParseError: {}", msg)){};
+};
+
+}  // namespace thogcc::errors
