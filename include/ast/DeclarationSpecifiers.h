aaa851dddf6693b09464cec300af00723b188882
georgerob801 <86919579+georgerob801@users.noreply.github.com>
2026-03-12 21:38:25 +0000

    fix: add type qualifiers
    not quite sure how we managed to miss quite literally the entire concept of type qualifiers? seems rather fundamental to the operation of a program lmfao

--- include/ast/DeclarationSpecifiers.h
+++ include/ast/DeclarationSpecifiers.h
@@ -5,26 +5,35 @@
 
 #include "ast/Node.h"
 #include "ast/StorageClassSpecifier.h"
+#include "ast/TypeQualifier.h"
 
 namespace thogcc::ast {
 
 class DeclarationSpecifiers : public Node {
    public:
     DeclarationSpecifiers(std::unique_ptr<ValueNode<StorageClassSpecifier>> storageClassSpecifier,
-                          std::unique_ptr<Node> typeSpecifier)
+                          std::unique_ptr<Node> typeSpecifier,
+                          std::unique_ptr<ValueNode<TypeQualifier>> typeQualifier)
         : _storageClassSpecifiers(std::make_unique<NodeList<ValueNode<StorageClassSpecifier>>>(
               std::move(storageClassSpecifier))),
-          _typeSpecifiers(std::make_unique<NodeList<Node>>(std::move(typeSpecifier))){};
+          _typeSpecifiers(std::make_unique<NodeList<Node>>(std::move(typeSpecifier))) {};
+
     void pushBackStorage(std::unique_ptr<ValueNode<StorageClassSpecifier>> storageClassSpecifier) {
         _storageClassSpecifiers->pushBack(std::move(storageClassSpecifier));
     };
-    void pushBackType(std::unique_ptr<Node> typeSpecifier) {
+
+    void pushBackTypeSpecifier(std::unique_ptr<Node> typeSpecifier) {
         _typeSpecifiers->pushBack(std::move(typeSpecifier));
     };
+
+    void pushBackTypeQualifier(std::unique_ptr<ValueNode<TypeQualifier>> typeQualifier) {
+        _typeQualifiers->pushBack(std::move(typeQualifier));
+    }
 
    private:
     std::unique_ptr<NodeList<ValueNode<StorageClassSpecifier>>> _storageClassSpecifiers;
     std::unique_ptr<NodeList<Node>> _typeSpecifiers;
+    std::unique_ptr<NodeList<ValueNode<TypeQualifier>>> _typeQualifiers;
 };
 
 }  // namespace thogcc::ast
