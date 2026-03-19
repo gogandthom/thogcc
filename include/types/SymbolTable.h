647ce335f6bc14b71ca3a34d7528e316d13efdd1
Thomas Aldrian <aldrian.thom@proton.me>
2026-03-19 19:13:09 +0000

    refactor: SymbolTable and Scope

--- include/types/SymbolTable.h
+++ include/types/SymbolTable.h
@@ -2,6 +2,7 @@
 
 #include <cassert>
 #include <format>
+#include <iostream>
 #include <map>
 #include <memory>
 #include <stdexcept>
@@ -9,55 +10,10 @@
 #include <variant>
 #include <vector>
 
+#include "types/Scope.h"
 #include "types/Type.h"
 
 namespace thogcc::types {
-
-struct VarSymbol {
-    std::shared_ptr<Type> type;
-};
-
-struct FuncSymbol {
-    std::shared_ptr<Type> type;
-    bool isDefinition;
-};
-
-struct TypedefSymbol {
-    std::shared_ptr<Type> type;
-};
-
-struct StructField {
-    std::string name;
-    std::shared_ptr<Type> type;
-};
-
-struct UnionField {
-    std::string name;
-    std::shared_ptr<Type> type;
-};
-
-struct StructSymbol {
-    std::vector<StructField> members;
-    bool isDefinition;
-};
-
-struct UnionSymbol {
-    std::vector<UnionField> members;
-    bool isDefinition;
-};
-
-struct EnumSymbol {
-    std::map<std::string, int> variants;
-    bool isDefinition;
-};
-
-typedef std::variant<VarSymbol, FuncSymbol, TypedefSymbol> OrdSymbol;
-typedef std::variant<StructSymbol, UnionSymbol, EnumSymbol> TagSymbol;
-
-struct Scope {
-    std::map<std::string, TagSymbol> tagSymbols;
-    std::map<std::string, OrdSymbol> ordinarySymbols;
-};
 
 class SymbolTable {
    public:
