e7e1a8001c0d3dea0acf19d884a5c5209e7a4fc1
Thomas Aldrian <aldrian.thom@proton.me>
2026-03-20 04:53:16 +0000

    refactor: remove unneccessary typedefs

--- include/ir/llvm.h
+++ include/ir/llvm.h
@@ -19,18 +19,18 @@ enum class LLVMValueKind : std::uint8_t {
     CONST,
 };
 
-typedef struct LLVMValueID {
+struct LLVMValueID {
     LLVMValueKind kind;
     int id;  // index into pool/'arena'
-} LLVMValueID;
+};
 
-typedef struct LLVMInstrID {
+struct LLVMInstrID {
     int id;
-} LLVMInstrID;
+};
 
-typedef struct LLVMBlockID {
+struct LLVMBlockID {
     int id;
-} LLVMBlockID;
+};
 
 #define LLVM_OPCODE                              \
     /* binary operations */                      \
@@ -82,40 +82,40 @@ inline std::string_view printOpcode(const LLVMOpcode& op) {
 };
 
 /// A set of instructions that runs start to finish without branching
-typedef struct LLVMBasicBlock {
+struct LLVMBasicBlock {
     std::string label;
     std::vector<LLVMInstrID> instrIDs;  // indexes into function 'instructions' pool
-} LLVMBasicBlock;
+};
 
 /// A single LLVM instruction
-typedef struct LLVMInstruction {
+struct LLVMInstruction {
     LLVMOpcode opcode;
     LLVMType type;
     std::vector<LLVMValueID> operands;
-} LLVMInstruction;
+};
 
 // Function parameters (we only support named parameters)
-typedef struct LLVMParameter {
+struct LLVMParameter {
     LLVMType type;
     std::string name;
-} LLVMParameter;
+};
 
 /// Constants
-typedef struct LLVMConstant {
+struct LLVMConstant {
     LLVMType type;
     std::variant<uint64_t, double> value;
-} LLVMConstant;
+};
 
 /// Global variable
-typedef struct LLVMGlobal {
+struct LLVMGlobal {
     LLVMType type;
     std::string name;
     std::variant<uint64_t, double> initValue;
-} LLVMGlobal;
+};
 
 /// A single function.
 /// Each LLVMFunction also contains its own instructions, params, consts arenas.
-typedef struct LLVMFunction {
+struct LLVMFunction {
     std::string name;
     LLVMType returnType;
     std::vector<LLVMBasicBlock> blocks;
@@ -153,13 +153,13 @@ typedef struct LLVMFunction {
         }
         throw std::runtime_error("Invalid ValueID");
     }
-} LLVMFunction;
+};
 
 /// The LLVM equivalent of a translational unit
-typedef struct LLVMModule {
+struct LLVMModule {
     std::string srcFileName;
     std::vector<LLVMFunction> functions;
     std::vector<LLVMGlobal> globals;
-} LLVMModule;
+};
 
 }  // namespace thogcc::ir
