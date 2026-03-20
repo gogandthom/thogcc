73833f0e9449d6d1bd23ecfb74e65c7c90c0b603
georgerob801 <86919579+georgerob801@users.noreply.github.com>
2026-03-20 14:58:12 +0000

    feat: addsub

--- src/codegen/RISCVEmitter.cpp
+++ src/codegen/RISCVEmitter.cpp
@@ -341,7 +341,6 @@ void RISCVEmitter::emitInstruction(ir::LLVMInstrID instrID) {
         case ir::LLVMOpcode::CALL:
             break;
         case ir::LLVMOpcode::RET:
-            std::cout << instr.operands.at(0).id << std::endl;
             loadValue(instr.operands.at(0), "a0");
             pushStack(instrID.id, "zero");
             _out << std::format("    j .L_{}_epilogue\n", _curFunc->name);
