e121b95d1ef4d7397d04a711f9009b84fa9abe57
Thomas Aldrian <aldrian.thom@proton.me>
2026-03-22 22:03:03 +0000

    refactor: make alloca more sane

--- src/codegen/RISCVEmitter.cpp
+++ src/codegen/RISCVEmitter.cpp
@@ -35,7 +35,16 @@ void RISCVEmitter::loadValue(const ir::LLVMValueID& valID, std::string_view targ
             }
             break;
         case ir::LLVMValueKind::INSTR:
-            loadFromStack(valID.id, targetReg);
+            // alloca instructions don't do anything at runtime
+            // we simply return the address of its stack slot
+            if (ir::getInstr(*_curFunc, {valID.id}).opcode == ir::LLVMOpcode::ALLOCA) {
+                _out << std::format("    addi {}, s0, -{}\n", targetReg, getSlotOffset(valID.id));
+            }
+            // and for regular instructions (temporary registers),
+            // we load the value from the stack
+            else {
+                loadFromStack(valID.id, targetReg);
+            }
             break;
         case ir::LLVMValueKind::CONST:
             std::visit(
@@ -313,8 +322,6 @@ void RISCVEmitter::emitInstruction(ir::LLVMInstrID instrID) {
             pushStack(instrID.id, "t2");
             break;
         case ir::LLVMOpcode::ALLOCA:
-            _out << std::format("    addi t0, s0, -{}\n", getSlotOffset(instrID.id));
-            pushStack(instrID.id, "t0");
             break;
         case ir::LLVMOpcode::LOAD:
             loadValue(instr.operands.at(0), "t0");
@@ -325,27 +332,24 @@ void RISCVEmitter::emitInstruction(ir::LLVMInstrID instrID) {
             loadValue(instr.operands.at(0), "t0");
             loadValue(instr.operands.at(1), "t1");
             _out << "    sw t0, 0(t1)\n";
-            pushStack(instrID.id, "zero");
             break;
         case ir::LLVMOpcode::BR:
             // unconditional jump
-            pushStack(instrID.id, "zero");
             if (instr.operands.size() == 1) {
                 _out << std::format("    j .L_{}_{}\n", _curFunc->name,
-                                    ir::getValueLabel(*_curFunc, instr.operands.at(0)));
+                                    ir::getValueLabel(_module, *_curFunc, instr.operands.at(0)));
             } else {
                 loadValue(instr.operands.at(0), "t0");
                 _out << std::format("    bnez t0, .L_{}_{}\n", _curFunc->name,
-                                    ir::getValueLabel(*_curFunc, instr.operands.at(1)));
+                                    ir::getValueLabel(_module, *_curFunc, instr.operands.at(1)));
                 _out << std::format("    j .L_{}_{}\n", _curFunc->name,
-                                    ir::getValueLabel(*_curFunc, instr.operands.at(2)));
+                                    ir::getValueLabel(_module, *_curFunc, instr.operands.at(2)));
             }
             break;
         case ir::LLVMOpcode::CALL:
             break;
         case ir::LLVMOpcode::RET:
             loadValue(instr.operands.at(0), "a0");
-            pushStack(instrID.id, "zero");
             _out << std::format("    j .L_{}_epilogue\n", _curFunc->name);
             break;
 
